b64985ccd8355b3b90558200d4d0d7d9359a7760
SAMYAK99 <singhsamyak199@gmail.com>
2020-07-23 21:12:16 +0530

    Added Login and SignUp Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firechat/Screens/WelcomeScreen.dart';
 import 'package:flutter/material.dart';
 
 import 'Screens/LoginScreen.dart';
@@ -18,6 +19,7 @@ class MyApp extends StatelessWidget {
 //        WelcomeScreen.id: (context) => WelcomeScreen(),
         RegistrationScreen.id: (context) => RegistrationScreen(),
         LoginScreen.id: (context) => LoginScreen(),
+        WelcomeScreen.id: (context) => WelcomeScreen(),
         //     ChatScreen.id: (context) => ChatScreen(),
       },
     );
